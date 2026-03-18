e483c43591ae39bb906536b403c2be032c7c6c4a
David Baquero <davidbaquerogonzalez@gmail.com>
2026-03-18 22:15:32 +0100

    Añadido código de la Live Review

--- src/App.jsx
+++ src/App.jsx
@@ -49,3 +49,62 @@ useEffect(() => {
 };
 
 export default App;
+
+
+// 👇 CODIGO DE LA LIVE REVIEW EMPIEZA DESDE AQUÍ 👇
+/* 
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from './Home.jsx'
+import ItemDetailPage from "./ItemDetailPage.jsx";
+import InputCreate from "./InputCreate.jsx";
+
+
+const App = () => {
+  const [data, setData] = useState(null)
+  const urlApi = import.meta.env.VITE_APP_API_URL || 'http://localhost:3000/'
+  const [update, setUpdate] = useState(false)
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(urlApi)
+    const resData = await response.json()
+    setData(resData)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+useEffect(() => {
+  fetchData()
+}, [update])
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Inicio </Link>
+          <Link to="/create"> Create</Link>
+     
+        </nav>
+        {data === null 
+        ? (<div>cargando...</div>) 
+        : 
+          <Routes>
+            <Route path="/" element={<Home data={data} />} />
+            <Route path="/create" element={<InputCreate setUpdate={setUpdate} />} />
+           
+            {data.map(item => (
+              <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item}/>} />
+            ))
+            }
+          </Routes>
+        }
+        
+      </div>
+    </Router>
+  )
+};
+
+export default App;
+ */
--- src/components/InputCreate.jsx
+++ src/components/InputCreate.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+// (prev => !prev) -> Significa: Si viene false, se cambia a true. Si viene true, se cambia a false
+
 function InputCreate() {
   const [inputValue, setInputValue] = useState('');
   const urlApi = 'http://localhost:3001/create';
@@ -47,3 +49,60 @@ function InputCreate() {
 }
 
 export default InputCreate;
+
+
+
+// 👇 CODIGO DE LA LIVE REVIEW EMPIEZA DESDE AQUÍ 👇
+/* 
+import { useState } from "react";
+
+function InputCreate ({setUpdate}) {
+  const [title, setTitle] = useState('')
+  const [res, setRes] = useState('Ready!!!')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const baseURL = import.meta.env.VITE_APP_API_URL || 'http://localhost:3000/'
+    const urlAPICreate = baseURL+'create'
+    const payload = {title}
+
+    try {
+      const response = await fetch(urlAPICreate, {
+        method: 'POST', // Método HTTP
+        headers: {
+          'Content-Type': 'application/json', // Indicamos que el contenido es JSON
+        },
+        body: JSON.stringify(payload), // Convertimos el payload de JS a JSON
+      })
+      if(response.ok) {
+        const data = await response.json()
+        setRes(`Enviado: ${data.title}`)
+        setTitle('')
+        setUpdate(prev => !prev)
+      } else {
+        throw new Error('se ha roto')
+      }
+    } catch (err) {
+      console.log('Error:', err)
+    }
+  }
+
+  return (
+  <>
+  <form onSubmit={handleSubmit}>
+    <input 
+    type="text"
+    placeholder="Escribe una tarea"
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}
+    />
+    <button type="submit">Enviar</button>
+  </form>
+  {res}
+  </>
+  )
+}
+
+export default InputCreate;
+ */
